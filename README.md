5ea2ae05907d2d5720243e26975715ef5a5c323e
Ethan Bayer <studioethanb@gmail.com>
2025-06-28 17:51:06 -0400

    Correct licensing info
    Add a licensing header to the top of every source code file.
    Add contact info to README.md.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # AutoCompressors
+
 A Spigot plugin that adds highly customizable item compressors.
+This plugin was made for Minecraft version 1.20.4.
+
+## Contact
+
+You can contact me at [studioethanb@gmail.com](mailto:studioethanb@gmail.com).
--- src/main/java/io/github/studioeb/autocompressors/items/CompressorItemManager.java
+++ src/main/java/io/github/studioeb/autocompressors/items/CompressorItemManager.java
@@ -1,3 +1,23 @@
+/*
+Static methods for managing compressors
+Copyright (C) 2025 Ethan Bayer
+
+This file is part of AutoCompressors.
+
+AutoCompressors is free software: you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+AutoCompressors is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package io.github.studioeb.autocompressors.items;
 
 import io.github.studioeb.autocompressors.AutoCompressors;
